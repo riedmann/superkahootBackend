beed7ca316ced64a9838490e8c09358252246fe3
andreas riedmann <andreas.riedmann@htldornbirn.at>
2026-01-07 10:24:28 +0100

    extracted firebase info

--- firebase.ts
+++ firebase.ts
@@ -2,19 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzsTw14bIm0rhQ4sMMKnn2Cki970q-AWM",
-  authDomain: "demoteachers.firebaseapp.com",
-  projectId: "demoteachers",
-  storageBucket: "demoteachers.firebasestorage.app",
-  messagingSenderId: "900017013166",
-  appId: "1:900017013166:web:5a5836b8f85e79c6c3483f",
-  measurementId: "G-BHWC9QZ4L6",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
